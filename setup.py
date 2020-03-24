ee188dfdf55eaef646f0cdc96700406f9c4cd5ec
Jacob Neil Taylor <me@jacobtaylor.id.au>
2020-03-24 15:42:31 +1100

    Increment version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="metaloader",
-    version="0.1.2",
+    version="0.1.5",
     author="Jacob Neil Taylor",
     author_email="me@jacobtaylor.id.au",
     description="A configurable pipeline for loading configuration files",
