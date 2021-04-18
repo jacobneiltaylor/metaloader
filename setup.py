8b2e9883747308b8cafdaf5b0c8a7932ff262474
Jacob Neil Taylor <me@jacobtaylor.id.au>
2021-04-18 17:02:53 +1000

    Increment version number and update build parameters

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="metaloader",
-    version="0.3.1",
+    version="0.4.0",
     author="Jacob Neil Taylor",
     author_email="me@jacobtaylor.id.au",
     description="A configurable pipeline for loading data files",
